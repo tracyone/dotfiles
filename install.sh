c53e3e3ef2b9d4e5c8da5c8af58e0d7996cecce8
tracyone <tracyone@live.cn>
2015-08-07 22:50:20 +0800

    更新安装脚本

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
 cur_dir=$(pwd)
 echo "Start install..."
-ln -s ${cur_dir}/.zshrc ${HOME}
-ln -s ${cur_dir}/.tmux.conf ${HOME}
-ln -s ${cur_dir}/.gitconfig ${HOME}
+ln -sf ${cur_dir}/.zshrc ${HOME}
+ln -sf ${cur_dir}/.tmux.conf ${HOME}
+ln -sf ${cur_dir}/.gitconfig ${HOME}
 sudo cp 10-monitor.conf /usr/share/X11/xorg.conf.d/
 echo "Install finish."
