268534301312f9bf4e45cd21b3591385e8b84718
tracyone <tracyone@live.cn>
2015-11-13 17:38:09 +0800

    install relate software first

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 cur_dir=$(pwd)
 echo "Start install..."
+sudo apt-get install zsh tmux git 
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 ln -sf ${cur_dir}/.zshrc ${HOME}
 ln -sf ${cur_dir}/.tmux.conf ${HOME}
 ln -sf ${cur_dir}/.gitconfig ${HOME}
