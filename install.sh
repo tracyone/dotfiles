73012e9edb60a837b5e0de2ba300a9b6e00059f8
tracyone <tracyone@live.cn>
2015-11-27 11:26:59 +0800

    操作系统相关

--- install.sh
+++ install.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
 OS=$(uname)
-
-
+echo OS is $OS
 cur_dir=$(pwd)
 echo "Start install..."
-sudo apt-get install zsh tmux git xclip
-sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+if [[ $OS == "Linux" ]] ;then
+	sudo apt-get install zsh tmux git xclip
+    sudo cp 10-monitor.conf /usr/share/X11/xorg.conf.d/
+elif [[ $OS == 'Darwin' ]]; then
+	brew install zsh tmux git xclip
+fi
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)" 
+
+if [[ $? -eq 0 && -d ~/.oh-my-zsh/plugins ]]; then
+	git clone https://github.com/jocelynmallon/zshmarks  ~/.oh-my-zsh/plugins/zshmarks && echo "Install zshmarks successfully!"
+else
+	echo "Error!Please install oh-my-zsh first."
+fi
+
 ln -sf ${cur_dir}/.zshrc ${HOME}
 ln -sf ${cur_dir}/.tmux.conf ${HOME}
 ln -sf ${cur_dir}/.gitconfig ${HOME}
-if [[ $OS == "Linux" ]] ;then
-    echo OS is $OS
-    sudo cp 10-monitor.conf /usr/share/X11/xorg.conf.d/
-fi
 echo "Install finish."
