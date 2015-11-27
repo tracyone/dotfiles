66e0e6ab1c46fb16f72fbe68c35e2fbfa21cce2e
tracyone <tracyone@live.cn>
2015-11-27 10:16:22 +0800

    增加操作系统的判断

--- install.sh
+++ install.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+OS=$(uname)
+
+
 cur_dir=$(pwd)
 echo "Start install..."
 ln -sf ${cur_dir}/.zshrc ${HOME}
 ln -sf ${cur_dir}/.tmux.conf ${HOME}
 ln -sf ${cur_dir}/.gitconfig ${HOME}
-sudo cp 10-monitor.conf /usr/share/X11/xorg.conf.d/
+if [[ $OS == "Linux" ]] ;then
+    echo OS is $OS
+    sudo cp 10-monitor.conf /usr/share/X11/xorg.conf.d/
+fi
 echo "Install finish."
