12ba7470a1b65a6253f292a79eb5dd5763ba0b2c
tracyone <tracyone@live.cn>
2016-03-27 21:52:41 +0800

    lesson 2

--- init.el
+++ init.el
@@ -19,6 +19,9 @@
                             company
                             monokai-theme
 			    hungry-delete
+			    smex
+			    swiper
+			    counsel
                             ) "Default packages" )
 
 (defun tracyone/packages-installed-p ()
@@ -72,15 +75,40 @@
 (global-hl-line-mode t)
 (load-theme 'monokai t)
 
+;;Plugin setting
 (require 'hungry-delete)
 (global-hungry-delete-mode)
+
+
+(require 'smex)
+;;(global-set-key (kbd "M-x") 'smex)
+
+;;smex simlar to ctrlp
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+(global-set-key "\C-s" 'swiper)
+(global-set-key (kbd "C-c C-r") 'ivy-resume)
+(global-set-key (kbd "<f6>") 'ivy-resume)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "<f1> f") 'counsel-describe-function)
+(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+(global-set-key (kbd "<f1> l") 'counsel-load-library)
+;;(global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+;;(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+(global-set-key (kbd "C-c g") 'counsel-git)
+(global-set-key (kbd "C-c j") 'counsel-git-grep)
+(global-set-key (kbd "C-c k") 'counsel-ag)
+(global-set-key (kbd "C-x l") 'counsel-locate)
+(global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (hungry-delete monokai-theme company))))
+ '(company-minimum-prefix-length 4)
+ '(package-selected-packages (quote (smex hungry-delete monokai-theme company))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
